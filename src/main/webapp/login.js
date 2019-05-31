1c46e68c93949e77a30a5a67fe95b5a159afb197
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-31 14:42:12 +0200

    Added functions for logging in and registering (no servlet supporting it yet, only the js functions)

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,12 +1,37 @@
+function onRegisterRedirectClicked() {
+    showContents(['container', 'login-content']);
+    loginTitleEl.textContent = 'Register';
+    hideContentById('login-form');
+    showContentById('register-form');
+}
+
+function onLoginRedirectClicked() {
+    showContents(['container', 'login-content']);
+    loginTitleEl.textContent = 'Login';
+    hideContentById('register-form');
+    showContentById('login-form');
+}
+
 function onLoginResponse() {
     if (this.status === OK) {
         const user = JSON.parse(this.responseText);
         setAuthorization(user);
         onProfileLoad(user);
     } else {
-        onOtherResponse(loginContentDivEl, this);
+        onOtherResponse(document.getElementById('login-form'), this);
     }
 }
+
+function onRegisterResponse() {
+    if (this.status === OK) {
+        const user = JSON.parse(this.responseText);
+        alert('Thank you for registering: ' + user.name);
+        onLoginRedirectClicked();
+    } else {
+        onOtherResponse(document.getElementById('register-form'), this);
+    }
+}
+
 
 function onLoginButtonClicked() {
     const loginFormEl = document.forms['login-form'];
@@ -25,5 +50,29 @@ function onLoginButtonClicked() {
     xhr.addEventListener('load', onLoginResponse);
     xhr.addEventListener('error', onNetworkError);
     xhr.open('POST', 'login');
+    xhr.send(params);
+}
+
+
+function onRegisterButtonClicked() {
+    const registerFormEl = document.forms['register-form'];
+
+    const nameInputEl = registerFormEl.querySelector('input[name="name"]');
+    const emailInputEl = registerFormEl.querySelector('input[name="email"]');
+    const passwordInputEl = registerFormEl.querySelector('input[name="password"]');
+
+    const name = nameInputEl.value;
+    const email = emailInputEl.value;
+    const password = passwordInputEl.value;
+
+    const params = new URLSearchParams();
+    params.append('name', name);
+    params.append('email', email);
+    params.append('password', password);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onRegisterResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('POST', 'register');
     xhr.send(params);
 }
