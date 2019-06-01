60b06369e7bc8ae9e6c2895efaa8cd5767858160
AndreaSzoboszlai <szobandi@gmail.com>
2019-06-01 17:01:37 +0200

    Added Servlet for displaying all order for employees

--- src/main/java/com/codecool/web/servlet/OrderServlet.java
+++ src/main/java/com/codecool/web/servlet/OrderServlet.java
@@ -1,4 +1,37 @@
 package com.codecool.web.servlet;
 
-public class OrderServlet {
+import com.codecool.web.dao.OrderDao;
+import com.codecool.web.dao.database.DatabaseOrderDao;
+import com.codecool.web.model.Order;
+import com.codecool.web.service.OrderService;
+import com.codecool.web.service.simple.SimpleOrderService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/protected/orders")
+public class OrderServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            OrderDao orderDao = new DatabaseOrderDao(connection);
+            OrderService orderService = new SimpleOrderService(orderDao);
+            List<Order> orders = orderService.findAll();
+
+            sendMessage(response, HttpServletResponse.SC_OK, orders);
+        } catch (SQLException ex) {
+            handleSqlError(response, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
 }
